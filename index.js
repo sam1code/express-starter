91a8f6363e18c6ca0cf81f474978e1d71c5089d1
soumya <samcse95@gmail.com>
2022-07-23 21:08:16 +0530

    done

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const app = express();
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.send(process.env.PORT);
+});
